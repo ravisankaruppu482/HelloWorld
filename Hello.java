f4b86182479f273d19765ed9cae4bdc6935ee321
ravisankaruppu482 <41979219+ravisankaruppu482@users.noreply.github.com>
2018-08-10 18:34:24 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 public static void main(String args[]){
 for(int i=1;i<=20;i++){
 System.out.println("Hello world from Devops");
+System.out.println("Hello world from Devops1");
 }
 }
 }
